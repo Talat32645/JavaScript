80ec95b53d9afba36875d2e92f294c435e7aa50b
Talat32645 <151937250+Talat32645@users.noreply.github.com>
2023-12-15 14:30:37 +0300

    Add files via upload

---
+++ JavaScript-1/java.js
@@ -0,0 +1,28 @@
+var user = prompt ('Введите своё имя')
+
+console.log ( ' Ваше имя ' + (user) );
+
+
+var year = +prompt ('Введите свой возраст')
+
+console.log ( ' Вам ' + (year) );
+
+var quizF = +prompt ('Пример Номер 1  6 + 3')
+
+console.log (' Пример Номер 1 :  6 + 3 = ' + + (6+3) + ' Ваш Ответ  ' + quizF );
+
+var quizS = +prompt ('Пример Номер 2  20 - 5 ')
+
+console.log (' Пример Номер 2 :  20 - 5 = ' + + (20-5) + ' Ваш Ответ  ' + quizS );
+
+var quizTH = +prompt ('Пример Номер 3  15 * 2 ')
+
+console.log (' Пример Номер 3 :  15 * 2 = ' + + (15*2) + ' Ваш Ответ  ' + quizTH );
+
+var quizFO = +prompt ('Пример Номер 2  8 / 4 ')
+
+console.log (' Пример Номер 4 :  8 / 4 = ' + + (8 / 4) + ' Ваш Ответ  ' + quizFO );
+
+var quizFI = +prompt ('Пример Номер 2  10 % 3 ')
+
+console.log (' Пример Номер 5 :  10 % 3 = ' + + (10 % 3) + ' Ваш Ответ  ' + quizFI );
